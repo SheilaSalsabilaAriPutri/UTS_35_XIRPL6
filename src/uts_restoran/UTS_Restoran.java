8cfed8676d7b7f81316d23114608f5a4da0c8513
Sheila Salsabila Ari Putri <sheilasalsabila46@gmail.com>
2017-03-15 11:58:49 +0800

    UTS

---
+++ src/uts_restoran/UTS_Restoran.java
@@ -0,0 +1,23 @@
+package uts_restoran;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Moklet
+ */
+public class UTS_Restoran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
